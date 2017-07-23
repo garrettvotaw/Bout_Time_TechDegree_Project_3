97a2313bcd1d0f7682a62adc99b1392993632dbd
garrettvotaw <garrettvotaw@users.noreply.github.com>
2017-07-23 16:06:37 -0500

    Initial Commit

---
+++ Bout Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bout Time
+//
+//  Created by Garrett Votaw on 7/23/17.
+//  Copyright © 2017 Garrett Votaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
