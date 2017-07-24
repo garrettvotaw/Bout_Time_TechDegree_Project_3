0b3cce576d1fee4b0c44db0aeeb3cec494d1e286
garrettvotaw <garrettvotaw@users.noreply.github.com>
2017-07-24 11:24:35 -0500

    Initial Commit

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(colorLiteralRed: 0, green: 41.0/255.0, blue: 75.0/255.0, alpha: 1)
+        textView.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
