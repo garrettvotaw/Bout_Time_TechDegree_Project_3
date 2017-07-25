725636c689b1e16f586552423b54c7fe612cfc6d
garrettvotaw <garrettvotaw@users.noreply.github.com>
2017-07-25 16:16:44 -0500

    UI Complete with reordering implemented

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -10,19 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var eventOneLabel: UILabel!
+    @IBOutlet weak var eventTwoLabel: UILabel!
+    @IBOutlet weak var eventThreeLabel: UILabel!
+    @IBOutlet weak var eventFourLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor(colorLiteralRed: 0, green: 41.0/255.0, blue: 75.0/255.0, alpha: 1)
-        textView.layer.cornerRadius = 5
+    }
+    
+    
+    
+    
+    @IBAction func downButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: swapValues(firstLabel: &eventOneLabel.text!, secondLabel: &eventTwoLabel.text!)
+        case 1: swapValues(firstLabel: &eventTwoLabel.text!, secondLabel: &eventThreeLabel.text!)
+        case 2: swapValues(firstLabel: &eventThreeLabel.text!, secondLabel: &eventFourLabel.text!)
+        default: break
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func upButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: swapValues(firstLabel: &eventTwoLabel.text!, secondLabel: &eventOneLabel.text!)
+        case 1: swapValues(firstLabel: &eventThreeLabel.text!, secondLabel: &eventTwoLabel.text!)
+        case 2: swapValues(firstLabel: &eventFourLabel.text!, secondLabel: &eventThreeLabel.text!)
+        default: break
+        }
     }
-
+    
+    //MARK: Helper Methods
+    func swapValues( firstLabel: inout String, secondLabel: inout String) {
+        var tempLabel: String
+        tempLabel = firstLabel
+        firstLabel = secondLabel
+        secondLabel = tempLabel
+    }
+    
+    
+    
+    
+    
 
 }
 
